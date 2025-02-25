42318f1852e8eb80b3aef604ab5c19265b6fa748
luisferreiraa <luis.c.ferreira.prt_a@msft.cesae.pt>
2025-02-25 19:49:57 +0000

    Commit

--- src/main/java/com/example/CRUDApplication/controller/AuthenticationController.java
+++ src/main/java/com/example/CRUDApplication/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.example.CRUDApplication.controller;
 
 import com.example.CRUDApplication.dto.LoginRequest;
+import com.example.CRUDApplication.dto.LoginResponseDTO;
+import com.example.CRUDApplication.model.User;
+import com.example.CRUDApplication.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +20,17 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid LoginRequest data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.getUsername(), data.getPassword());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
 
     }
 
---
+++ src/main/java/com/example/CRUDApplication/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.CRUDApplication.dto;
+
+public record LoginResponseDTO(String token) {
+}
